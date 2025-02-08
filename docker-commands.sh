8b3e4fd1851a47de4963f0c54f2dd4fc1d4dd9b9
jrolla777 <75419447+jrolla777@users.noreply.github.com>
2025-02-07 21:52:10 -0500

    Update docker-commands.sh

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,54 +1,50 @@
 #!/bin/sh
+
+# Define variables
 cwd=$(pwd)
-defd='~/compose/'
-targd=''
-name=''
-if [ -z "$1" ]
- then
-  targd=$cwd
+compose_dir=~/compose
+container_folder=""
+container_name=""
 
- else
-  targd="${cwd}/${1}"
-  mkdir -p $targd
-  name=$1
-
+# Determine the target directory and container name
+if [ -z "$1" ]; then
+  container_folder=$cwd
+  container_name=$(basename "$cwd")
+else
+  container_folder="${cwd}/${1}"
+  container_name="$1"
 fi
 
+# Ensure target directory exists
+mkdir -p "$container_folder"
 
-rm -f $targd/restart.sh
-touch $targd/restart.sh
-{
-        echo '#!/bin/sh'
-        echo 'docker compose stop'
-        echo 'docker compose up -d'
+# Function to create scripts
+create_script() {
+  script_name="$1"
+  content="$2"
+  script_path="$container_folder/$script_name"
 
-} >> $targd/restart.sh
+  rm -f "$script_path"
+  touch "$script_path"
+  chmod +x "$script_path"
 
-rm -f $targd/top.sh
-touch $targd/top.sh
-{
-        echo '#!/bin/sh'
-       # echo 'cid=$(docker ps | grep -i $name | awk '{print $1}')'
-        echo "cid=\$(docker ps | grep -i $name | awk '{print \$1}')"
-        echo 'docker exec -it $cid top'
+  echo "#!/bin/sh" > "$script_path"
+  echo "$content" >> "$script_path"
+}
 
-} >> $targd/top.sh
+# Restart script
+create_script "restart.sh" "docker compose stop && docker compose up -d"
 
-rm -f $targd/update-container.sh
-touch $targd/update-container.sh
-{
-        echo '#!/bin/sh'
-        echo '#docker compose stop'
-        echo 'docker compose down'
-        echo 'docker compose pull'
-        echo 'docker compose up -d'
+# Update container script
+create_script "update-container.sh" "docker compose stop && docker compose pull && docker compose up -d"
 
-} >> $targd/update-container.sh
+# Rebuild container script
+create_script "rebuild-container.sh" "docker compose down && docker compose pull && docker compose up -d"
 
-## see conainer processes
-rm -f $targd/top.sh
-touch $targd/top.sh
- { echo '#!/bin/sh'
-   echo "cid=\$(docker ps | grep -i ${name} | awk '{print \$1}')"
-   echo 'docker exec -it $cid top'
- } >> $targd/top.sh
+# Top script (view running processes)
+create_script "top.sh" "cid=\$(docker ps | grep -i $container_name | awk '{print \$1}')\ndocker exec -it \$cid top"
+
+# Exec script with flexible container name
+create_script "exec.sh" "cid=\$(docker ps | grep -i $container_name | awk '{print \$1}')\ndocker exec -it \$cid sh"
+
+echo "Scripts have been created in $container_folder"
